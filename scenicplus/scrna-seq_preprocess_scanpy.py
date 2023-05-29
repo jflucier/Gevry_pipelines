e870c769e90e7355114e9975e94e7c312a5fe36f
jflucier <3505568+jflucier@users.noreply.github.com>
2023-05-29 11:04:28 -0400

    added tracing in script exec

--- scenicplus/scrna-seq_preprocess_scanpy.py
+++ scenicplus/scrna-seq_preprocess_scanpy.py
@@ -10,17 +10,21 @@ def run_scrna_precprocess(work_dir, in_data, mtx_dir, mito_filter, n_counts_filt
     # scRNA-seq preprocessing using Scanpy
     sc.settings.set_figure_params(dpi=80, frameon=False, figsize=(5, 5), facecolor='white')
 
+    print(f"initialisation of variable and output folder {work_dir}/scRNA")
     if not os.path.exists(os.path.join(work_dir, 'scRNA')):
         os.makedirs(os.path.join(work_dir, 'scRNA'))
 
+    print(f"reading input 10x h5 file")
     adata = sc.read_10x_h5(in_data)
     # adata.var_names_make_unique()
 
     # Basic quality control
+    print(f"filtering input by min_genes {min_genes} & min_cells {min_cells}")
     sc.pp.filter_cells(adata, min_genes=min_genes)
     sc.pp.filter_genes(adata, min_cells=min_cells)
     # Optionally, predict and filter out doublets using Scrublet.
     # estimates doublets
+    print(f"Predicting and filtering out doublets using Scrublet.")
     sc.external.pp.scrublet(adata)
     # do the actual filtering
     adata = adata[adata.obs['predicted_doublet'] == False]
@@ -28,9 +32,11 @@ def run_scrna_precprocess(work_dir, in_data, mtx_dir, mito_filter, n_counts_filt
 
     # Filter based on mitochondrial counts and total counts.
     # annotate the group of mitochondrial genes as 'mt'
+    print(f"Annotating the group of mitochondrial genes as 'mt'")
     adata.var['mt'] = adata.var_names.str.startswith('MT-')
     sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
 
+    print(f"Generating figure qc_counts_filter_mt{mito_filter}_cnt{n_counts_filter}.png")
     fig, axs = plt.subplots(ncols=2, figsize=(8, 4))
     sc.pl.scatter(adata, x='total_counts', y='pct_counts_mt', ax=axs[0], show=False)
     sc.pl.scatter(adata, x='total_counts', y='n_genes_by_counts', ax=axs[1], show=False)
@@ -40,11 +46,14 @@ def run_scrna_precprocess(work_dir, in_data, mtx_dir, mito_filter, n_counts_filt
     fig.tight_layout()
     plt.savefig(f"{work_dir}/qc_counts_filter_mt{mito_filter}_cnt{n_counts_filter}.png")
 
+    print(f"Filtering all count data by total counts {n_counts_filter} and mitochondrial counts {mito_filter}")
     adata = adata[adata.obs.n_genes_by_counts < n_counts_filter, :]
     adata = adata[adata.obs.pct_counts_mt < mito_filter, :]
 
     # OPTIONNAL??? Data normalization for visualisation purpose
+    print(f"Backup filtered data.")
     adata.raw = adata
+    print(f"Performing data normalisation")
     sc.pp.normalize_total(adata, target_sum=1e4)
     sc.pp.log1p(adata)
     sc.pp.highly_variable_genes(adata, min_mean=min_mean, max_mean=max_mean, min_disp=min_disp)
@@ -54,6 +63,7 @@ def run_scrna_precprocess(work_dir, in_data, mtx_dir, mito_filter, n_counts_filt
     # Cell type annotation
     # use the preprocessed data from the Scanpy tutorial as reference
     # adata_ref = sc.datasets.pbmc3k_processed()
+    print(f"Perform cell type annotation using mtx dir {mtx_dir}")
     adata_ref = sc.read_10x_mtx(
         # the directory with the `.mtx` file. Tuto data taken from
         # https://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_raw_gene_bc_matrices.tar.gz
@@ -67,6 +77,7 @@ def run_scrna_precprocess(work_dir, in_data, mtx_dir, mito_filter, n_counts_filt
     # adata.write('write/pbmc3k_raw.h5ad', compression='gzip')
 
     # use genes which are present in both assays
+    print(f"Crossing count data with annotation data using gene symbols")
     var_names = adata_ref.var_names.intersection(adata.var_names)
     adata_ref = adata_ref[:, var_names]
     adata = adata[:, var_names]
@@ -81,20 +92,24 @@ def run_scrna_precprocess(work_dir, in_data, mtx_dir, mito_filter, n_counts_filt
     adata.obs.rename({'louvain': 'ingest_celltype_label'}, inplace=True, axis=1)
 
     # Let’s visualize the labels
+    print(f"Generate cell type annotation plot cell_type_annotation_variance_ratio.png")
     sc.tl.pca(adata, svd_solver='arpack')
     sc.pl.pca_variance_ratio(adata, log=True)
     plt.savefig(work_dir + '/cell_type_annotation_variance_ratio.png')
 
+    print(f"Generate cell subgroups plot by label cell_subgroups.label.png")
     sc.pp.neighbors(adata, n_neighbors=n_neighbors, n_pcs=10)
     sc.tl.umap(adata)
     sc.pl.umap(adata, color='ingest_celltype_label')
     plt.savefig(work_dir + '/cell_subgroups.label.png')
 
     # Cluster cells into subgroups using the Leiden algorithm
+    print(f"Clustering cells into subgroups using the Leiden algorithm")
     sc.tl.leiden(adata, resolution=leiden_res, key_added=f'leiden_res_{leiden_res}')
     sc.pl.umap(adata, color=f'leiden_res_{leiden_res}')
     plt.savefig(f'{work_dir}/cell_subgroups.leiden{leiden_res}.png')
 
+    print(f"TEST: reannotate some groups")
     tmp_df = adata.obs.groupby([f'leiden_res_{leiden_res}', 'ingest_celltype_label']).size().unstack(fill_value=0)
     tmp_df = (tmp_df / tmp_df.sum(0)).fillna(0)
     leiden_to_annotation = tmp_df.idxmax(1).to_dict()
@@ -105,12 +120,13 @@ def run_scrna_precprocess(work_dir, in_data, mtx_dir, mito_filter, n_counts_filt
     leiden_to_annotation = {cluster: leiden_to_annotation[cluster].replace(' ', '_') for cluster in
                             leiden_to_annotation.keys()}
     # leiden_to_annotation
-
+    print(f"Generate cell subgroups plot by cell type cell_subgroups.celltype.png")
     adata.obs['celltype'] = [leiden_to_annotation[cluster_id] for cluster_id in adata.obs['leiden_res_0.8']]
     sc.pl.umap(adata, color='celltype')
     plt.savefig(work_dir + '/cell_subgroups.celltype.png', bbox_inches="tight")
 
     # Save results
+    print(f"Saving data object in {work_dir}/scRNA/adata.h5ad")
     adata.write(os.path.join(work_dir, 'scRNA/adata.h5ad'), compression='gzip')
 
 
